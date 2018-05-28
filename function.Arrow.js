98ffc1e5b6dd302cacdcc1ac27a3bb84a585f96a
Student <Student>
2018-05-28 17:38:37 +0300

    first commit

---
+++ function.Arrow.js
@@ -0,0 +1,18 @@
+function give_back(student){
+
+	var student = ["caren","babra","anca","yvnn","susan","Purity","Sandra",]
+
+
+
+	return student[i]
+
+
+}
+
+result = return()
+
+console.log(result)
+
+
+const answer = (["caren","babra","anca","yvnn","susan","Purity","Sandra") => {return student[i]};
+
